0f53d195956461732f5cc4dd737ef5759deee7a6
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-19 16:20:32 -0300

    refatoracao: finalizacao da primeira versao da criacao da tarefa do usuario.

--- apps/frontend/src/pages/todo-list/createTask.tsx
+++ apps/frontend/src/pages/todo-list/createTask.tsx
@@ -1,24 +1,47 @@
-import { useForm } from "react-hook-form"
-import { type TaskInterface } from "../../interfaces";
-interface CreateTaskProps{
-    createTask: () => void;
-}
+import type { TaskInterface, NewTaskInterface } from "../../../../../packages/schemas/taskInterfaces";
+import * as Dialog from "@radix-ui/react-dialog";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
-export function CreateTask(props: CreateTaskProps){
-const {
-    register,
-    handleSubmit
-} = useForm<TaskInterface>();
+export function CreateTask(props: NewTaskInterface){
+const [isOpen, setIsOpen] = useState(false);
+
+const { register, handleSubmit } = useForm<TaskInterface>();
+
+const onSubmit = handleSubmit(async (task: TaskInterface) => {
+    try{
+        props.createTask(task)
+        setIsOpen(false);
+    } catch(error){
+        console.error(error);
+    }
+});
 
 return (
-    <form onSubmit={handleSubmit(props.createTask)}>
-        <label htmlFor="titleTaskInput">Titulo da atividade</label>
-        <input type="text" id="titleTaskInput" {...register("title", {required: true, minLength: 4})}/>
+    <Dialog.Root open={ isOpen } onOpenChange={ setIsOpen }>
+        <Dialog.Trigger className="bg-blue-500">
+            Criar nova tarefa
+        </Dialog.Trigger>
 
-        <label htmlFor="contentTaskInput">Conteudo da atividade</label>
-        <input type="text" id="contentTaskInput" {...register("content", {required: true, minLength: 4})}/>
+        <Dialog.Content className="">
+            <Dialog.Title className="">
+                Nova tarefa
+            </Dialog.Title>
 
-        <input type="submit" value="Criar atividade"/>
-    </form>
+            <form onSubmit={onSubmit} className="flex flex-col gap-1 items-center">
+                <label htmlFor="titleTaskInput">Titulo da atividade</label>
+                <input type="text" id="titleTaskInput" {...register("title", {required: "Titulo é um campo obrigatório!", minLength: 4})}/>
+
+                <label htmlFor="contentTaskInput">Conteudo da atividade</label>
+                <input type="text" id="contentTaskInput" {...register("content", {required: "Deve ser escrito o conteudo da tarefa!", minLength: 4})}/>
+                
+                <input type="submit" value="Criar atividade"/>
+            </form>
+
+            <Dialog.Close className="">
+                Cancelar
+            </Dialog.Close>
+        </Dialog.Content>
+    </Dialog.Root>
 )
 }
