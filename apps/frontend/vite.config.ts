73c546392d511242cb2d631cf8799ba1a8b3ee58
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-20 01:21:33 -0300

    refatoracao: reorganizacao das importacoes.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -11,5 +10,5 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     })
-  ],
+  ]
 })
