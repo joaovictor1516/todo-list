ede283b2f422db7c755f1302038fb8fb7e8f8986
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-20 21:19:43 -0300

    atualizacao: instalacao e inportacao do Toaster no src/main.tsx.

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
+import { Toaster } from "sonner";
 import { App } from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster/>
     <App/>
   </StrictMode>
 )
