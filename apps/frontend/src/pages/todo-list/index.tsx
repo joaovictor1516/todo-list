a531cfd177ead57fb58742bf87eaca737a8ff255
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-22 10:07:47 -0300

    desenvolvimento basico do funcionamento da pagina de lesta de tarefas.

--- apps/frontend/src/pages/todo-list/index.tsx
+++ apps/frontend/src/pages/todo-list/index.tsx
@@ -1,5 +1,81 @@
+import type { TaskInterface } from "../../../../../packages/schemas/taskInterfaces";
+import { useState } from "react";
+import { CreateTask } from "./createTask";
+import { toast } from "sonner";
+import { Task } from "./task";
+
 export function TodoList(){
+    const [tasks, setTasks] = useState<TaskInterface[]>([]);
+    const [tasksCompleted, setTasksCompleted] = useState<TaskInterface[]>([]);
+
+    const createTask = async (newTask: TaskInterface) => {
+        const task: TaskInterface = {
+            id: crypto.randomUUID(),
+            title: newTask.title,
+            content: newTask.content,
+            isCompleted: false
+        }
+
+        setTasks([...tasks, task]);
+
+        toast.success("Atividade criada com sucesso!", {
+            duration: 5000,
+        })
+    }
+
+    const completeTask = (taskId: string) => {
+        const completedTodo = tasks.find((task) => task.id === taskId);
+        const newTaskList = tasks.filter((task) => task.id !== taskId);
+        
+        if(newTaskList){
+            setTasks([...newTaskList]);
+        }
+
+        if(completedTodo){
+            const updateTodo = {...completedTodo, isCompleted: true}
+            setTasksCompleted([updateTodo,...tasksCompleted]);
+        }
+        console.log("foi");
+    }
+
     return(
-        <></>
+        <div className="">
+            <CreateTask createTask={createTask}/>
+            <h1 className="">Tarefas:</h1>
+            
+            {
+                tasks.length > 0 ?
+                    tasks.map((task) => {
+                        return(
+                            <Task 
+                                key={task.id} 
+                                {...task}
+                                completeTask={() => completeTask(task.id)}
+                            />
+                        )
+                    }) :
+                
+                    (
+                        <span className="">Nenhuma tarefa criada :(</span>
+                    )
+            }
+            
+            <h1 className="">Concluido:</h1>
+            {
+                tasksCompleted.length > 0 ? 
+                    tasksCompleted.map((task) => {
+                        return(
+                            <Task 
+                                key={task.id}
+                                {...task}
+                            />
+                        )
+                    })
+
+                    :(
+                        <span className="">Nenhuma tarefa foi concluida :(</span>
+                    )
+            }
+        </div>
     )
 }
