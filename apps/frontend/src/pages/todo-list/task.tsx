3930927b0c0463b0d3c3e4fe434646cf804e675e
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-22 16:37:05 -0300

    refatoracao: adicao dos icones de Trash2, para o botao de deletar a tarefa, e de Check, para o botao de conclusao da tarefa.

--- apps/frontend/src/pages/todo-list/task.tsx
+++ apps/frontend/src/pages/todo-list/task.tsx
@@ -1,4 +1,5 @@
 import type { TaskInterface } from "../../../../../packages/schemas/taskInterfaces";
+import { Check, Trash2 } from "lucide-react"
 
 export function Task(props: TaskInterface){
     return(
@@ -7,14 +8,14 @@ export function Task(props: TaskInterface){
             <p className="">{props.content}</p>
             {
                 props.completeTask ? (
-                    <button type="button" className="" onClick={() => props.completeTask(props.id)}>Concluir</button>
+                    <button type="button" className="" onClick={() => props.completeTask(props.id)}><Check/></button>
                 ) : (
-                    <button type="button" className="bg-green-500">Concluir</button>
+                    <button type="button" className="text-green-500"><Check/></button>
                 )
             }
             
             <button type="button" className="">Editar</button>
-            <button type="button" className="" onClick={() => props.deleteTask(props.id)}>Excluir</button>
+            <button type="button" className="text-red-600" onClick={() => props.deleteTask(props.id)}><Trash2/></button>
         </div>
     )
 }
