13549aec627a43c7cb22bb4ef9b0e41e35c33548
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-23 09:47:21 -0300

    remocao dos arquivos auth.ts e deleteTask.ts que foi notado que nao serao mais necessarios pois as suas funcoes ja foram criadas no createTask.tsx, no caso do auth.ts, e index.tsx, no caso do deleteTask.ts.

--- apps/frontend/src/pages/todo-list/auth.ts
+++
@@ -1,9 +0,0 @@
-import type { NewTaskInterface, TaskInterface } from "../../../../../packages/schemas/taskInterfaces";
-
-export function authNewTask(newTask: NewTaskInterface){
-    try {
-        newTask.createTask(task);
-    } catch(error) {
-        console.error(error);
-    }
-}
