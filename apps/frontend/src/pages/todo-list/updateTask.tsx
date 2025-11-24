df12bd5b5acc50cfc23483394343345bb1a6d728
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-11-24 10:52:25 -0300

    adicao dos ids e dos botoes de salvar e cancelar.

--- apps/frontend/src/pages/todo-list/updateTask.tsx
+++ apps/frontend/src/pages/todo-list/updateTask.tsx
@@ -31,9 +31,17 @@ export function UpdateTask(props: TaskInterface){
                 </Dialog.Title>
 
                 <form className="" onSubmit={save}>
-                    <label htmlFor="" className="">Titulo</label>
-                    <input type="text" className="" content={props.title} {...register("title")}/>
+                    <label htmlFor="title" className="">Titulo</label>
+                    <input type="text" id="title" className="" content={props.title} {...register("title")}/>
+
+                    <label htmlFor="content" className="">Conteudo:</label>
+                    <input type="text" id="content" className="" content={props.content} {...register("content")}/>
+
+                    <input type="submit" value="Salvar" className=""/>
                 </form>
+                <Dialog.Close className="">
+                    Cancelar
+                </Dialog.Close>
             </Dialog.Content>
         </Dialog.Root>
     )
