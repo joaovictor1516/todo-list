be7a64e6286828c39d8d06ff28327c876266bea4
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-12-23 20:42:37 -0300

    refatoracao: desenvolvimento inicial da pagina de erros da aplicacao.

--- apps/frontend/src/pages/error/index.tsx
+++ apps/frontend/src/pages/error/index.tsx
@@ -1,5 +1,16 @@
+import { useRouteError } from "react-router-dom"
+
 export function Error(){
+    interface ErrorProps{
+        message: string;
+    }
+
+    const error = useRouteError() as ErrorProps;
+    console.error(error);
+
     return (
-        <></>
+        <div className="text-center">
+            <h1 className="">404</h1>
+        </div>
     )
 }
