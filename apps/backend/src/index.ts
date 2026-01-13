08db694bc3f8090fd8d50ee5be3b2ee034df3d25
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-01-13 17:12:30 -0300

    refatoracao: substituicao da porta e da url do front, pelos seus valores salvos no .env.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,16 +1,17 @@
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { env } from "./libs/envSchema";
 import cors from "@fastify/cors";
 import fastify from "fastify";
 
 const app = fastify();
 
 app.register(cors, {
-    origin: "http://localhost:3030/"
+    origin: env.WEB_BASE_URL
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.listen({
-    port: 3000
+    port: env.PORT
 });
