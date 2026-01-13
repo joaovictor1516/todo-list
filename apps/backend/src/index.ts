1a51cc4d0c2cd49719e7e96d2b6248a74355eac2
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-01-13 19:31:33 -0300

    adicao da documentacao da api.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,28 @@
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastifySwaggerUI from "@fastify/swagger-ui"
+import fastifySwagger from "@fastify/swagger";
 import { env } from "./libs/envSchema";
 import cors from "@fastify/cors";
 import fastify from "fastify";
 
 const app = fastify();
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ["aplication/json"],
+        produces: ["aplication/json"],
+        info: {
+            title: "DailyTasks",
+            description: "API da aplicação de tarefas.",
+            version: "1.0.0"
+        }
+    },
+    transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUI, {
+    routePrefix: "/docs"
+});
 
 app.register(cors, {
     origin: env.WEB_BASE_URL
