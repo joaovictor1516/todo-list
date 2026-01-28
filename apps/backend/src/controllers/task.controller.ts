1a6837539838b2b310292207b5de133190951533
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-01-28 17:05:03 -0300

    desenvolvimento do controller das tasks.

--- apps/backend/src/controllers/task.controller.ts
+++ apps/backend/src/controllers/task.controller.ts
@@ -1,3 +1,4 @@
+import { TaskInterface } from "../../../../packages/schemas/taskInterfaces";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function createTask(request: FastifyRequest, resply: FastifyReply){
@@ -6,14 +7,53 @@ export async function createTask(request: FastifyRequest, resply: FastifyReply){
     resply.code(201).send({task});
 }
 
-export async function deleteTask(request: FastifyRequest, reply: FastifyReply){
+export async function getTasks(request: FastifyRequest, reply: FastifyReply) {
 
 }
 
-export async function getTasks(request: FastifyRequest, reply: FastifyReply){
-    
+export async function deleteTask(request: FastifyRequest<
+        {
+            Params: {
+                id: string
+            }
+        }
+    >, reply: FastifyReply){
+        const id = request.params.id;
+
+        reply.code(201);
 }
 
-export async function getTaskById(request: FastifyRequest, reply: FastifyReply){
-    
+export async function getTaskById(request: FastifyRequest<
+        {
+            Params: {
+                id: string
+            }    
+        }
+    >, reply: FastifyReply){
+
+        const id = request.params.id;
+
+        reply.code(201);
+
+}
+
+export async function updateTask(request: FastifyRequest<
+        {
+            Params: { 
+                id: string 
+            },
+            Body: {
+                task: TaskInterface
+            }
+        }
+    >, reply: FastifyReply){
+
+        const id = request.params.id;
+        const task = request.body.task;
+
+        if(id !== task.id){
+            reply.code(400).send({
+                message: "Task not find"
+            });
+        }
 }
