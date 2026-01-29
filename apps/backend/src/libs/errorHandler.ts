77596858bb239b83ff6ebdedca451cdf7b988b89
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-01-29 16:30:21 -0300

    criacao da lib errorHandler, para padronizar os erros do back-end.

---
+++ apps/backend/src/libs/errorHandler.ts
@@ -0,0 +1,18 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { ZodError } from "zod";
+
+export function errorHandler(error: unknown, request: FastifyRequest, reply: FastifyReply){
+    if(error instanceof ZodError){
+        return reply.code(400).send({
+            message: "Type error"
+        });
+    } else if(error instanceof Error){
+        return reply.code(400).send({
+            message: error.message
+        });
+    } else {
+        return reply.code(500).send({
+            message: "Internal error"
+        });
+    }
+}
