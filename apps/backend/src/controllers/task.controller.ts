9ed7e50b3daa08b8d8205727fdc92e7ea9c15f26
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-01-31 01:05:53 -0300

    adicao do Body no request das funcoes createTask e getTasks, alem da adicao do return na condicional do ID buscado ser diferente da task passada na funcao updateTask.

--- apps/backend/src/controllers/task.controller.ts
+++ apps/backend/src/controllers/task.controller.ts
@@ -1,14 +1,28 @@
 import { TaskInterface } from "../../../../packages/schemas/taskInterfaces";
 import { FastifyRequest, FastifyReply } from "fastify";
 
-export async function createTask(request: FastifyRequest, resply: FastifyReply){
-    const task = request.body;
+export async function createTask(request: FastifyRequest<
+        {
+            Body: {
+                task: TaskInterface
+            }
+        }
+    >, resply: FastifyReply){
+        const task = request.body.task;
 
-    resply.code(201).send({task});
+        resply.code(201).send({task});
 }
 
-export async function getTasks(request: FastifyRequest, reply: FastifyReply) {
+export async function getTasks(request: FastifyRequest<
+        {
+            Body: {
+                tasks: TaskInterface[]
+            }
+        }
+    >, reply: FastifyReply) {
+        const tasks = request.body.tasks;
 
+        reply.code(201).send(tasks);
 }
 
 export async function deleteTask(request: FastifyRequest<
@@ -52,7 +66,7 @@ export async function updateTask(request: FastifyRequest<
         const task = request.body.task;
 
         if(id !== task.id){
-            reply.code(400).send({
+            return reply.code(400).send({
                 message: "Task not find"
             });
         }
