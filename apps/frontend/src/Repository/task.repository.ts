1c2f7644c47747885e06d2e9762bbc2b5f6f56d9
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-02-06 10:05:54 -0300

    criacao do repository do task.

---
+++ apps/frontend/src/Repository/task.repository.ts
@@ -0,0 +1,49 @@
+import { getTasksApi, createTaskApi, updateTaskApi, deleteTaskApi } from "../api/task.api";
+import type { TaskInterface } from "../../../../packages/schemas/taskInterfaces";
+import type { repository } from "../../../../packages/schemas/apiInterfaces";
+
+export class OffLineTaskRepository implements repository<TaskInterface> {
+    async getAll(): Promise<TaskInterface[]> {
+        const tasks = localStorage.getItem("tasks");
+        return tasks ? JSON.parse(tasks) : [];
+    }
+
+    async create(task: TaskInterface): Promise<TaskInterface> {
+        const tasks = await this.getAll();
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        return task;
+    }
+
+    async update(id: string, task: TaskInterface): Promise<TaskInterface> {
+        const tasks = await this.getAll();
+        const taskIndex = tasks.findIndex((task) => task.id === id);
+        tasks[taskIndex] = task
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        return task
+    }
+
+    async delete(id: string): Promise<void> {
+        const tasks = await this.getAll();
+        const newTasks = tasks.filter((task) => task.id !== id);
+        localStorage.setItem("tasks", JSON.stringify(newTasks));
+    }
+}
+
+export class OnLineTaskRepository implements repository<TaskInterface>{
+    async getAll():Promise<TaskInterface[]>{
+        return getTasksApi();
+    }
+
+    async create(task: TaskInterface){
+        return createTaskApi(task);
+    }
+
+    async update(id: string, task: TaskInterface){
+        return updateTaskApi(id, task);
+    }
+
+    async delete(id: string){
+        return deleteTaskApi(id);
+    }
+}
