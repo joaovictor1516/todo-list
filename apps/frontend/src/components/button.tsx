1c6b19276cdc740a399ca40d5e2fa5fcd42afade
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-02-10 22:02:19 -0300

    inicio da criacao da logica do componente do botao.

--- apps/frontend/src/components/button.tsx
+++ apps/frontend/src/components/button.tsx
@@ -1,3 +1,32 @@
-export function Button(){
-    
+import { tv, type VariantProps } from "tailwind-variants";
+import type { ComponentProps, ReactNode } from "react";
+
+const buttonStyle = tv({
+    base: "flex items-center justify-center gap-2 rounded-md font-mediun hover:text-white",
+    variants: {
+        colors: {
+            primary: "text-gren-500 hover:bg-gren-500",
+            secubdary: "text-red-500  hover:bg-gren500"
+        },
+        size: {
+            default: "",
+            big : ""
+        }
+    },
+    defaultVariants: {
+        colors: "primary",
+        size: "default"
+    }
+});
+
+interface ButtonProps extends ComponentProps<"button">, VariantProps<typeof buttonStyle>{
+    children: ReactNode;
+}
+
+export function Button({children, colors, size, ...props}: Readonly<ButtonProps>){
+    return(
+        <button {...props} className={buttonStyle({colors, size})}>
+            {children}
+        </button>
+    )
 }
