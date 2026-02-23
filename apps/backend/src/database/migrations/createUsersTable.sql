506158f3b10656228435ea38d54050fc88402d1e
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-02-23 13:38:17 -0300

    adaicao do user_password e definicao da quantidade do varchar do user_name e do user_email.

--- apps/backend/src/database/migrations/createUsersTable.sql
+++ apps/backend/src/database/migrations/createUsersTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     user_id UUID PRIMARY KEY,
-    user_email VARCHAR() UNIQUE NOT NULL,
-    user_name VARCHAR() UNIQUE NOT NULL,
+    user_password TEXT NOT NULL,
+    user_email VARCHAR(254) UNIQUE NOT NULL,
+    user_name VARCHAR(50) UNIQUE NOT NULL,
     user_points INT NOT NULL DEFAULT 0,
 );
