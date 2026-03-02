146d8438553c9a2eabfb654c65c32205ccd73e87
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-03-02 12:24:46 -0300

    criacao do controller de autenticao do usuario.

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -1,11 +1,21 @@
-import { UserInterface } from "../../../../packages/schemas/userInterfaces";
+import { UserInputDto, UserLoginDto } from "../../../../packages/schemas/userInterfaces";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { AuthService } from "../service/auth.service";
 
-export async function authCreateUser(request: FastifyRequest, reply: FastifyReply){
-    const userTaked = request.body as UserInterface;
-    const authService = new AuthService();
-    const user = await authService.register(userTaked);
+export class AuthController{
+    async authCreateUser(request: FastifyRequest, reply: FastifyReply) {
+        const userTaked = request.body as UserInputDto;
+        const authService = new AuthService();
+        const user = await authService.register(userTaked);
 
-    reply.code(201).send(user);
+        reply.code(201).send(user);
+    }
+
+    async authLoginUser(request: FastifyRequest, reply: FastifyReply){
+        const loginUserInformations = request.body as UserLoginDto;
+        const authService = new AuthService();
+        const user = await authService.login(loginUserInformations);
+
+        reply.code(201).send(user);
+    }
 }
