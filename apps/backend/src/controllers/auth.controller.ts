41de1e7ded6bfa4254f18c52a2bd9b9c8171c7fd
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-03-02 21:30:37 -0300

    refatoracao: melhoria do programa, adicao do private authService, recebendo o AuthService como tipagem, para que seja reuilizada nas funcoes.

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -3,19 +3,19 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { AuthService } from "../service/auth.service";
 
 export class AuthController{
+    constructor(private authService: AuthService){}
+
     async authCreateUser(request: FastifyRequest, reply: FastifyReply) {
         const userTaked = request.body as UserInputDto;
-        const authService = new AuthService();
-        const user = await authService.register(userTaked);
+        const user = await this.authService.register(userTaked);
 
         reply.code(201).send(user);
     }
 
     async authLoginUser(request: FastifyRequest, reply: FastifyReply){
         const loginUserInformations = request.body as UserLoginDto;
-        const authService = new AuthService();
-        const user = await authService.login(loginUserInformations);
+        const user = await this.authService.login(loginUserInformations);
 
-        reply.code(201).send(user);
+        reply.code(200).send(user);
     }
 }
