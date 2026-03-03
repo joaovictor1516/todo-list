0c63dc6429d38cff729ec71f2f7c30cdb446999a
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-03-03 09:22:17 -0300

    alteracao da tipagem, de TaskInterface para TaskDbBto.

--- apps/backend/src/repository/task.repository.ts
+++ apps/backend/src/repository/task.repository.ts
@@ -1,9 +1,9 @@
-import { TaskInterface } from "../../../../packages/schemas/taskInterfaces";
+import { TaskDbDto } from "../../../../packages/schemas/taskInterfaces";
 import { pool } from "../database";
 
 //Lembrar que todas as checagens de igualdade entre ID's sera feita no service!!!
 export class TaskRepository {
-    async createTask(task: TaskInterface): Promise<TaskInterface> {
+    async createTask(task: TaskDbDto): Promise<TaskDbDto> {
         const newTask = await pool.query("INSERT INTO tasks (task_id, task_title, task_content, task_state, task_created_at, task_event_date, task_priority)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
             [task.id, task.title, task.content, task.isCompleted, task.createdAt, task.eventDate, task.priority]
         );
@@ -11,19 +11,19 @@ export class TaskRepository {
         return newTask.rows[0];
     }
 
-    async getTasks(): Promise<TaskInterface[]> {
+    async getTasks(): Promise<TaskDbDto[]> {
         const tasks = await pool.query("SELECT * FROM tasks");
 
         return tasks.rows;
     }
 
-    async getTaskById(id: string): Promise<TaskInterface | undefined> {
+    async getTaskById(id: string): Promise<TaskDbDto | undefined> {
         const task = await pool.query("SELECT * FROM tasks WHERE task_id = $1", [id]);
 
         return task.rows[0];
     }
 
-    async updateTask(id: string, task: TaskInterface): Promise<TaskInterface> {
+    async updateTask(id: string, task: TaskDbDto): Promise<TaskDbDto> {
         const taskUpdated = await pool.query("UPDATE tasks SET task_title = $2, task_content = $3, task_state = $4, task_event_date = $5, task_priority = $6 WHERE task_id = $1 RETURNING *",
             [id, task.title, task.content, task.isCompleted, task.eventDate, task.priority]
         );
