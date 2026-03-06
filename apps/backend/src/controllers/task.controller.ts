06bb7f6508eef9c4f04636e6ada7ab980f89cf48
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-03-05 22:15:15 -0300

    refatoracao: no arquivo task.controller.ts, presente no back-end da aplicacao, foi criada a classe TaskController com o objetivo de melhorar na organizacao da aplicacao. essa classe deve receber a classe TaskService, pois as funcoes presentes nela serao usadas nas funcoes da classe TaskController.

--- apps/backend/src/controllers/task.controller.ts
+++ apps/backend/src/controllers/task.controller.ts
@@ -1,67 +1,74 @@
 import { TaskInterface } from "../../../../packages/schemas/taskInterfaces";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { TaskService} from "../service/task.service";
 
-export async function createTask(request: FastifyRequest<
+export class TaskController{
+    constructor(private taskService: TaskService){}
+    
+    async createTask(request: FastifyRequest<
         {
             Body: TaskInterface
         }
-    >, resply: FastifyReply){
-        const task = request.body;
+    >, resply: FastifyReply) {
+        const newTask = request.body;
+        const task = await this.taskService.createTask(newTask);
 
-        resply.code(201).send({task});
-}
+        resply.code(201).send(task);
+    }
 
-export async function getTasks(request: FastifyRequest<
-        {
-            Body: TaskInterface[]
-        }
-    >, reply: FastifyReply) {
-        const tasks = request.body;
+    async getTasks(request: FastifyRequest, reply: FastifyReply) {
+        const tasks = await this.taskService.getTasks();
 
         reply.code(201).send(tasks);
-}
+    }
 
-export async function deleteTask(request: FastifyRequest<
+    async deleteTask(request: FastifyRequest<
         {
             Params: {
                 id: string
             }
         }
-    >, reply: FastifyReply){
+    >, reply: FastifyReply) {
         const id = request.params.id;
 
         reply.code(201);
-}
+    }
 
-export async function getTaskById(request: FastifyRequest<
+    async getTaskById(request: FastifyRequest<
         {
             Params: {
                 id: string
-            }    
+            }
         }
-    >, reply: FastifyReply){
+    >, reply: FastifyReply) {
 
         const id = request.params.id;
+        const task = await this.taskService.getTaskById(id);
 
-        reply.code(201);
-
-}
-
-export async function updateTask(request: FastifyRequest<
-        {
-            Params: { 
-                id: string 
-            },
-            Body: TaskInterface
+        if(!task){
+            return reply.code(400).send({message: "Task bot founded."});
         }
-    >, reply: FastifyReply){
+
+        reply.code(201).send(task);
+
+    }
+
+    async updateTask(request: FastifyRequest<
+    {
+        Params: {
+            id: string
+        },
+        Body: TaskInterface
+    }
+    >, reply: FastifyReply) {
 
         const id = request.params.id;
         const task = request.body;
 
-        if(id !== task.id){
+        if (id !== task.id) {
             return reply.code(400).send({
                 message: "Task not find"
             });
         }
+    }
 }
