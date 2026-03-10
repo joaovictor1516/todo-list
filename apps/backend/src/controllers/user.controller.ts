c8ee34a94e49d850f2f5be1023934b982858f3df
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-03-10 14:54:14 -0300

    refatoracao do controller do user. criacao da fucao para atualizar o usuario.

--- apps/backend/src/controllers/user.controller.ts
+++ apps/backend/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { UserInterface, UserDbDto, UserInputDto } from "../../../../packages/schemas/userInterfaces";
+import { TaskDbDto } from "../../../../packages/schemas/taskInterfaces";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { UserService } from "../service/user.service";
 
@@ -30,4 +31,19 @@ export class UserController {
 
         return reply.code(200).send(userTasks);
     }
+
+    async updateUser(request: FastifyRequest<
+            {
+                Body: UserInputDto,
+                Params: {
+                    id: string
+                }
+            }
+        >, reply: FastifyReply){
+            const id = request.params.id;
+            const userInformations = request.body;
+            const userUpdated = await this.userService.userUpdate(id, userInformations);
+
+            return reply.code(201).send(userUpdated);
+        }
 }
