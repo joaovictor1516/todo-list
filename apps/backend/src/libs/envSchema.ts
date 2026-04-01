f335130588510b2981f4840995cabdaa70bae212
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-03-31 22:45:55 -0300

    refatoracao do envSchema do back-end, foi removido o API_BASE_URL e o DATA_BASE.

--- apps/backend/src/libs/envSchema.ts
+++ apps/backend/src/libs/envSchema.ts
@@ -2,11 +2,9 @@ import z from "zod";
 
 const envSchema = z.object({
     JWT_SECRET: z.string(),
-    API_BASE_URL: z.url(),
     WEB_BASE_URL: z.url(),
-    DATA_BASE: z.url(),
-    PORT: z.number(),
-    DATABASE_PORT: z.number(),
+    PORT: z.coerce.number(),
+    DATABASE_PORT: z.coerce.number(),
     DATABASE_PASSWORD: z.string(),
     DATABASE_NAME: z.string(),
     DATABASE_USER: z.string(),
