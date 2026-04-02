7810069d375533fc7bf9485a32f6f9e9d5513201
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-04-02 12:06:40 -0300

    refatoracao no mock do repository, foram alterados os nomes da funcoes, alem da criacao do interface expecifico para as funcoes.

--- apps/backend/tests/mock/user/user.repository.mock.ts
+++ apps/backend/tests/mock/user/user.repository.mock.ts
@@ -1,4 +1,13 @@
-export const userRepositoryMock = () => ({
-    findByEmail: jest.fn(),
-    findById: jest.fn(),
+import { UserDbDto } from "../../../../../packages/schemas/userInterfaces";
+import { TaskDbDto } from "../../../../../packages/schemas/taskInterfaces";
+import { jest } from "@jest/globals";
+
+interface UserRepositoryMockInterface {
+    getUserTasks: (id: string) => Promise<TaskDbDto | null>;
+    getUserById: (id: string) => Promise<UserDbDto | null>;
+};
+
+export const userRepositoryMock = (): jest.Mocked<UserRepositoryMockInterface> => ({
+    getUserTasks: jest.fn(),
+    getUserById: jest.fn(),
 });
