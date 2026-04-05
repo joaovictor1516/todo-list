1c91cf0f669a37a4c8f0c210deb46d42534129fe
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-04-05 13:06:30 -0300

    refatoracao no mock do repository das tasks. foi adicionado o TaskRepository como tipagem do mock para tornar os testes mais proximos as execucoes do programa em um cenario de funcionamento real.

--- apps/backend/tests/mock/task/task.repository.mock.ts
+++ apps/backend/tests/mock/task/task.repository.mock.ts
@@ -1,16 +1,11 @@
-import { TaskDbDto, TaskInterface } from "../../../../../packages/schemas/taskInterfaces";
+import { TaskRepository } from "../../../src/repository/task.repository";
 import { jest } from "@jest/globals";
 
-interface TaskRepositoryMockInterface {
-    createTask: (task: TaskInterface) => Promise<TaskDbDto>;
-    getTasks: () => Promise<TaskDbDto[]>;
-    getTaskById: (id: string) => Promise<TaskDbDto>;
-    checkTask: (id: string) => Promise<TaskDbDto>;
-};
-
-export const taskRepositoryMock = (): jest.Mocked<TaskRepositoryMockInterface> => ({
+export const taskRepositoryMock = (): jest.Mocked<TaskRepository> => ({
     createTask: jest.fn(),
     getTasks: jest.fn(),
     getTaskById: jest.fn(),
-    checkTask: jest.fn()
+    updateTask: jest.fn(),
+    checkTask: jest.fn(),
+    deleteTask: jest.fn()
 });
