f95eb1c2d618a8251f4da0a9156b9155e9776fd8
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-04-05 13:14:45 -0300

    refatoracao do arquivo de teste unitario do service do user. foi usado o beforeEach para inicializar o repository e o service antes de cada teste alem disso as funcoes do mock foram adicionadas dentro de cada teste para evitar conflitos.

--- apps/backend/tests/unit/user.service.test.ts
+++ apps/backend/tests/unit/user.service.test.ts
@@ -1,35 +1,29 @@
+import { describe, jest, test, expect, beforeEach } from "@jest/globals";
 import { userRepositoryMock } from "../mock/user/user.repository.mock";
+import { UserRepository } from "../../src/repository/user.repository";
 import { UserService } from "../../src/service/user.service";
-import { describe, test, expect } from "@jest/globals";
 
 describe("User service tests.", () => {
-    const repository = userRepositoryMock();
-    
-    const date = new Date();
-    
-    repository.getUserById.mockResolvedValue({
-        id: "1",
-        name: "Joao Victor",
-        email: "jvcampos531@gmail.com",
-        passwordHash: "algo",
-        points: 0,
-        createdAt: date
+    let repository: jest.Mocked<UserRepository>;
+    let service: UserService;
+
+    beforeEach(() => {
+        repository = userRepositoryMock();
+
+        service = new UserService(repository as any);
     });
 
-    repository.getUserTasks.mockResolvedValue([{
-        id: "1",
-        title: "Estudar Node.JS",
-        content: "Estudar bibliotecas usadas para subir migrations do banco de dados.",
-        createdAt: date,
-        lastUpdate: date,
-        eventDate: date,
-        priority: "high",
-        isCompleted: false
-    }]);
-
-    const service = new UserService(repository as any);
-
     test("Get a user using his id:", async () => {
+        const date = new Date();
+
+        repository.getUserById.mockResolvedValue({
+            id: "1",
+            name: "Joao Victor",
+            email: "jvcampos531@gmail.com",
+            passwordHash: "algo",
+            points: 0,
+            createdAt: date
+        });
         const result =  await service.getUserInformations("1");
 
         expect(result).toEqual({
@@ -43,6 +37,38 @@ describe("User service tests.", () => {
     });
 
     test("Get user tasks:", async () => {
+        const date = new Date();
+        const dateUser = new Date();
+        
+        repository.getUserById.mockResolvedValue({
+            id: "1",
+            name: "Joao Victor",
+            email: "jvcampos531@gmail.com",
+            passwordHash: "algo",
+            points: 0,
+            createdAt: dateUser
+        });
+
+        repository.getUserTasks.mockResolvedValue([{
+            id: "1",
+            title: "Estudar Node.JS",
+            content: "Estudar bibliotecas usadas para subir migrations do banco de dados.",
+            createdAt: date,
+            lastUpdate: date,
+            eventDate: date,
+            priority: "high",
+            isCompleted: false
+        }, {
+            id: "2",
+            title: "Ir para a academia",
+            content: "Criar vergonha na cara e melhorar a saude.",
+            createdAt: date,
+            lastUpdate: date,
+            eventDate: date,
+            priority: "medium",
+            isCompleted: false
+        }]);
+
         const result = await service.getUserTasks("1");
 
         expect(result).toEqual([{
@@ -54,12 +80,19 @@ describe("User service tests.", () => {
             eventDate: date,
             priority: "high",
             isCompleted: false
+        }, {
+            id: "2",
+            title: "Ir para a academia",
+            content: "Criar vergonha na cara e melhorar a saude.",
+            createdAt: date,
+            lastUpdate: date,
+            eventDate: date,
+            priority: "medium",
+            isCompleted: false
         }]);
     });
 
     test("User not founded.", async () => {
-        repository.getUserById.mockResolvedValue(null);
-
         await expect(service.getUserInformations("2"))
             .rejects
             .toThrow("User not founded.");
