eedb19df133779b062083709be1a5cb441222837
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-04-14 17:00:53 -0300

    alteracao das migrations de sql para ts, e sua refatoracao para a implementacao da biblioteca tests/database/migrations.

--- apps/backend/tests/database/migrations/002.createUserTestTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(
-    user_id UUID PRIMARY KEY,
-    user_password TEXT NOT NULL,
-    user_email VARCHAR(254) NOT NULL,
-    user_name VARCHAR(50) UNIQUE NOT NULL,
-    user_points INT NOT NULL DEFAULT 0
-)
