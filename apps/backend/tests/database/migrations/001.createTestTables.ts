f64a53c07fb51da3479a8c6f83a52c63d4dc444e
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-05-14 09:57:00 -0300

    refatoracao dos arquivos migration do bd dos testes: remocao das antigas migrations que estavam em .sql, e foi criada a migration que cria as duas tabelas, frita em ts para ser usada com o drizzle-orm.

---
+++ apps/backend/tests/database/migrations/001.createTestTables.ts
@@ -0,0 +1,21 @@
+import { pgTable, uuid, varchar, text, integer, timestamp, boolean } from "drizzle-orm/pg-core";
+
+export const userTableTest = pgTable("usersTest", {
+    user_id: uuid().defaultRandom().primaryKey(),
+    user_email: varchar({length: 254}).unique().notNull(),
+    user_password: text().notNull(),
+    user_name: varchar({length: 50}).unique().notNull(),
+    user_points: integer().notNull().default(0),
+    user_created_at: timestamp().notNull().defaultNow()
+});
+
+export const taskTableTest = pgTable("tasksTest", {
+    task_id: uuid().defaultRandom().primaryKey(),
+    task_title: varchar({length: 25}).notNull(),
+    task_content: varchar({length: 100}).notNull(),
+    task_is_completed: boolean().notNull().default(false),
+    task_created_at: timestamp().notNull().defaultNow(),
+    task_event_date: timestamp(),
+    task_priority: varchar({length: 6}).notNull(),
+    user_id: uuid().defaultRandom().references(() => userTableTest.user_id)
+});
