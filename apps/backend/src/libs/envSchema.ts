ce067b77f66b0687d90d8fe1a3f14a0e12507e18
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-05-20 09:24:00 -0300

    refatoracao da biblioteca envSchema do back-end, foi removido o DATABASE_TEST_NAME.

--- apps/backend/src/libs/envSchema.ts
+++ apps/backend/src/libs/envSchema.ts
@@ -8,8 +8,7 @@ const envSchema = z.object({
     DATABASE_PASSWORD: z.string(),
     DATABASE_NAME: z.string(),
     DATABASE_USER: z.string(),
-    DATABASE_HOST: z.string(),
-    DATABASE_TEST_NAME: z.string()
+    DATABASE_HOST: z.string()
 });
 
 export const env = envSchema.parse(process.env);
