f2cf15ce1cb8f4c33b4ccd367e498b8dd4a9d892
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-05-21 19:20:36 -0300

    refatoracao no migration de criacao de tabelas, os nomes referentes as tabelas foram alterados para serem iguais aos nomes dos campos dos schemas, mas os nomes das colunas das tabelas ainda seguirao o padrao de nomenclatura usado no sql.

--- apps/backend/src/database/migrations/001.createTables.ts
+++ apps/backend/src/database/migrations/001.createTables.ts
@@ -1,24 +1,47 @@
 import { pgTable, uuid, varchar, text, integer, boolean, timestamp } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("users", {
-    user_id: uuid().defaultRandom().primaryKey(),
-    user_email: varchar({length: 254}).unique().notNull(),
-    user_password: text().notNull(),
-    user_name: varchar({length: 50}).unique().notNull(),
-    user_points: integer().notNull().default(0),
-    user_created_at: timestamp().notNull().defaultNow()
+    id: uuid("id").defaultRandom().primaryKey(),
+    
+    email: varchar("email", {length: 254}).unique().notNull(),
+    
+    password: text("password").notNull(),
+    
+    name: varchar("name", {length: 50}).unique().notNull(),
+    
+    points: integer("points").notNull(),
+    
+    createdAt: timestamp("created_at", {
+        mode: "date"
+    }).notNull()
 });
 
 export const taskTable = pgTable("tasks", {
-    task_id: uuid().defaultRandom().primaryKey(),
-    task_title: varchar({length: 25}).notNull(),
-    task_content: varchar({length: 100}).notNull(),
-    task_is_completed: boolean().notNull().default(false),
-    task_created_at: timestamp().notNull().defaultNow(),
-    task_last_update: timestamp(),
-    task_event_date: timestamp(),
-    task_priority: varchar({length: 6}).notNull(),
-    user_id: uuid().defaultRandom().references(() => userTable.user_id, {
+    id: uuid("id").defaultRandom().primaryKey(),
+    
+    title: varchar("title", {length: 25}).notNull(),
+    
+    content: varchar("content", {length: 100}).notNull(),
+
+    isCompleted: boolean("is_completed").notNull(),
+    
+    createdAt: timestamp("created_at", {
+        mode: "date"
+    }).notNull(),
+
+    lastUpdate: timestamp("last_update", {
+        mode: "date"
+    }).notNull(),
+    
+    eventDate: timestamp("event_date", {
+        mode: "date"
+    }).notNull(),
+    
+    priority: text("priority", { 
+        enum: ["low", "medium", "high"]
+    }).notNull(),
+    
+    userId: uuid("id").references(() => userTable.id, {
         onDelete: "cascade",
         onUpdate: "cascade"
     })
