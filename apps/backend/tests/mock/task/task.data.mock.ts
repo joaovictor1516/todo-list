0196296dc89e45b4aed0980a687e0436c7afd563
joaovictor1516 <voliveira.joao30@sempreceub.com>
2026-05-30 00:32:29 -0300

    test: criacao da funcao de mock que sera usada nos testes de integracao das tasks.

--- apps/backend/tests/mock/task/task.data.mock.ts
+++ apps/backend/tests/mock/task/task.data.mock.ts
@@ -1,4 +1,5 @@
 import { TaskDbDto } from "../../../../../packages/schemas/taskInterfaces";
+import { randomUUID } from "node:crypto";
 
 export const taskDataMock = (overrides: Partial<TaskDbDto> = {}): TaskDbDto => ({
     id: "1",
@@ -12,3 +13,16 @@ export const taskDataMock = (overrides: Partial<TaskDbDto> = {}): TaskDbDto => (
     userId: "1",
     ...overrides
 });
+
+export const taskIntegrationDataMock = (overrides: Partial<TaskDbDto> = {}): TaskDbDto => ({
+    id: randomUUID(),
+    title: "Estudar Programcao",
+    content: "Estudar estrutura de dados.",
+    createdAt: new Date("2024-01-01T00:00:00.000Z"),
+    eventDate: new Date("2024-01-01T00:00:00.000Z"),
+    lastUpdate: new Date("2024-01-01T00:00:00.000Z"),
+    isCompleted: false,
+    priority: "high",
+    userId: randomUUID(),
+    ...overrides
+})
